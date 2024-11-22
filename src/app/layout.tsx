c917c1668552a2ce1365c45781ce69cb135f8e80
C_C_Project <153931545+CCusernameProject@users.noreply.github.com>
2024-11-22 07:35:00 +0100

    Fix: Add openGraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,18 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Montre connectée FitLife Pro 5+",
   description: "Boostez votre quotidien avec la FitLife Pro 5+ ! Une montre connectée ultra-polyvalente dotée d’un écran AMOLED HD, d’un suivi précis de vos activités sportives, de vos données de santé et d’une autonomie record de 10 jours.",
+  openGraph: {
+    title: "Montre connectée FitLife Pro 5+",
+    description: "Boostez votre quotidien avec la FitLife Pro 5+ ! Une montre connectée ultra-polyvalente dotée d’un écran AMOLED HD, d’un suivi précis de vos activités sportives, de vos données de santé et d’une autonomie record de 10 jours.",
+    images: [
+      {
+        url: "/Prevview_website.png",
+        alt: "Montre connectée FitLife Pro 5+",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
